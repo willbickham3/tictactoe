f63d161fb6326d1d3e584d8f02154c237d7f31c5
Will <willbickham3@gmail.com>
2023-05-08 15:14:37 -0400

    Change element from div to button, Fill button with array values

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const gameBoard = (() => {
-    const board = ['', '', '', '', '', '', '', '', ''];
+    const board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
     return {board};
 })();
 
 const populateBoard = (() => {
     for (let i = 0; i < gameBoard.board.length; i++) {
-        const square = document.createElement("div");
+        const square = document.createElement("button");
         square.classList.add('square');
-        document.body.appendChild(square)
+        square.innerText += gameBoard.board[i];
+        document.body.appendChild(square);
     }
 })();
 console.log(gameBoard.board.length)
