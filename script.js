2c4da78aa716e0f64c0ff4d7cb5da158d0eea672
Will <willbickham3@gmail.com>
2023-05-09 14:32:37 -0400

    updateBoard function to update board array and innertext on click

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const gameBoard = (() => {
     const board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
-    return {board};
+    const updateBoard = (index, value) => {
+        board[index] = value
+    }
+    return { board, updateBoard };
 })();
 
 const populateBoard = (() => {
     for (let i = 0; i < gameBoard.board.length; i++) {
         const square = document.createElement("button");
         square.classList.add('square');
-        square.innerText += gameBoard.board[i];
+        square.innerText = gameBoard.board[i];
+        square.addEventListener('click', () => {
+            const Player = 'X';
+            gameBoard.updateBoard(i, Player);
+            square.innerText = Player;
+        })
         document.body.appendChild(square);
     }
 })();
